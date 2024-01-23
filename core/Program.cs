55818366b0e54821f2a3431558638f1178d21e48
Jonathan Duval-Venne <duvalvenne.jonathan@gmail.com>
2024-01-23 15:26:35 -0500

    DI extension

--- core/Program.cs
+++ core/Program.cs
@@ -1,3 +1,5 @@
+using api.core.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDependencyInjection();
 
 var app = builder.Build();
 
