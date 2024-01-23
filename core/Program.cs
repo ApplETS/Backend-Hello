6521d4e86ba11e822ab91b6aa102867ca05537a0
Jonathan Duval-Venne <duvalvenne.jonathan@gmail.com>
2024-01-23 15:27:51 -0500

    Swagger auth

--- core/Program.cs
+++ core/Program.cs
@@ -6,6 +6,7 @@ using api.core.data;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,7 +29,34 @@ builder.Services.AddAuthentication().AddJwtBearer(o =>
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. " +
+        "\r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+         {
+               new OpenApiSecurityScheme
+               {
+                     Reference = new OpenApiReference
+                     {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                     }
+               },
+         new string[] {}
+         }
+     });
+});
 builder.Services.AddDependencyInjection();
 
 var app = builder.Build();
