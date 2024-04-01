269415cf87513bba1a67c9bf951ca56702b7b3e9
Hugo Migner <hugo.migner@gmail.com>
2024-04-01 10:00:02 -0400

    Create migration

---
+++ core/Migrations/20240401134414_AddUserHasLoggedIn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserHasLoggedIn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasLoggedIn",
+                table: "Organizer",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasLoggedIn",
+                table: "Organizer");
+        }
+    }
+}
