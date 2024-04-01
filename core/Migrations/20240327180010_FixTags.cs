17e72a914d5fe93442d2d4bb1e45e855b3029fb7
Samuel Montambault <mont.samuel@outlook.com>
2024-04-01 11:45:44 -0400

    fix tags ordering (#30)

---
+++ core/Migrations/20240327180010_FixTags.cs
@@ -0,0 +1,35 @@
+﻿using api.core.Migrations.SeededData;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.core.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            for (int i = 0; i < TagSeed0Initial.TagValues.GetLength(0); i++)
+            {
+                Guid id = (Guid)TagSeed0Initial.TagValues[i, 0];
+                migrationBuilder.DeleteData("Tag", "Id", id);
+            }
+
+            migrationBuilder.InsertData("Tag", TagSeed20240327180010.Columns, TagSeed20240327180010.TagValues);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            for (int i = 0; i < TagSeed20240327180010.TagValues.GetLength(0); i++)
+            {
+                Guid id = (Guid)TagSeed20240327180010.TagValues[i, 0];
+                migrationBuilder.DeleteData("Tag", "Id", id);
+            }
+            migrationBuilder.InsertData("Tag", TagSeed0Initial.Columns, TagSeed0Initial.TagValues);
+        }
+    }
+}
