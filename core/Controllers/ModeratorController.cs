454cc25a188b9521135b1236b799c18b254c040b
MysticFragilist <mont.samuel@outlook.com>
2024-04-04 22:06:25 -0400

    moderator

---
+++ core/Controllers/ModeratorController.cs
@@ -0,0 +1,35 @@
+﻿using api.core.Data;
+using api.core.Data.Requests;
+using api.core.services.abstractions;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.core.Controllers;
+
+[ApiController]
+[Route("api/moderator")]
+public class ModeratorController(IModeratorService moderatorService) : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateModerator([FromHeader(Name = "x-api-key")] string? accessKey, [FromBody] ModeratorCreateRequestDTO moderator)
+    {
+        // Create the moderator
+        try
+        {
+            if (accessKey == null)
+                throw new Exception("Access key not defined, can't trigger this function");
+
+            var moderatorRes = moderatorService.CreateModerator(accessKey, moderator);
+
+            return Ok(new Response<ModeratorResponseDTO>
+            {
+                Data = moderatorRes
+            });
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ core/Services/ModeratorService.cs
@@ -0,0 +1,37 @@
+﻿using api.core.data.entities;
+using api.core.Data.Exceptions;
+using api.core.Data.Requests;
+using api.core.repositories.abstractions;
+using api.core.services.abstractions;
+
+namespace api.core.Services;
+
+public class ModeratorService(IConfiguration configuration, IModeratorRepository moderatorRepository) : IModeratorService
+{
+    public ModeratorResponseDTO CreateModerator(string apiKey, ModeratorCreateRequestDTO req)
+    {
+        var adminAccessKey = Environment.GetEnvironmentVariable("ADMIN_ACCESS_API_KEY");
+        if (adminAccessKey == null)
+            throw new Exception("Admin access key not defined, can't trigger this function");
+
+        if (apiKey != adminAccessKey)
+            throw new UnauthorizedException();
+
+        // Create the moderator
+        moderatorRepository.Add(new Moderator
+        {
+            Id = req.Id,
+            Email = req.Email,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        });
+
+        return new ModeratorResponseDTO
+        {
+            Id = req.Id,
+            Email = req.Email,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+    }
+}
