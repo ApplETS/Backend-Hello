d9d329cd0114c2aae1bf815446d3caddfd63cc18
MysticFragilist <mont.samuel@outlook.com>
2024-06-26 13:14:04 -0400

    fix state

--- core/Data/Enums/State.cs
+++ core/Data/Enums/State.cs
@@ -14,11 +14,11 @@ namespace api.core.Data.Enums;
 [Flags]
 public enum State
 {
-    OnHold = 1, // 000001
-    Deleted = 2, // 000010
-    Denied = 4, // 000100
-    Approved = 8, // 001000
-    Published = 16, // 010000
-    Draft = 32, // 100000
+    OnHold = 1 << 0, // 000001
+    Deleted = 1 << 1, // 000010
+    Denied = 1 << 2, // 000100
+    Approved = 1 << 3, // 001000
+    Published = 1 << 4, // 010000
+    Draft = 1 << 5, // 100000
     All = OnHold | Deleted | Denied | Approved | Published | Draft // 111111
 }
