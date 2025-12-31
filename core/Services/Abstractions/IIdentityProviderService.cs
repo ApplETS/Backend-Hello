4a4bf09261e7dc653ba2f104549bcbdc137435be
superjekk <superjekk@outlook.com>
2025-12-30 23:04:14 -0500

    FCT : Service d'appel du fournisseur d'Identité
    Un service d'appel des fournisseurs d'Identité a été créé afin de pouvoir interroger le serveur d'Identité sans passer par le client

---
+++ core/Services/Abstractions/IIdentityProviderService.cs
@@ -0,0 +1,26 @@
+﻿namespace api.core.Services.Abstractions;
+
+/// <summary>
+/// Interface to communicate with any ID Provider
+/// </summary>
+public interface IIdentityProviderService
+{
+    /// <summary>
+    /// Fetches User's information from the ID Provider
+    /// </summary>
+    /// <param name="accessHeader">Header used to call the endpoint</param>
+    /// <returns>If result is null, UserInfo endpoint cannot be accessed</returns>
+    UserInfoDto? GetUserInfo(string accessHeader);
+}
+
+public class UserInfoDto
+{
+    public string Sub { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public bool EmailVerified { get; set; }
+    public string Name { get; set; } = null!;
+    public string GivenName { get; set; } = null!;
+    public string PreferedUsername { get; set; } = null!;
+    public string Nickname { get; set; } = null!;
+    public List<string> Groups { get; set; } = null!;
+}
---
+++ core/Services/AuthentikService.cs
@@ -0,0 +1,32 @@
+﻿using api.core.Services.Abstractions;
+
+using System.Text.Json;
+
+namespace api.core.Services;
+
+/// <summary>
+/// Service used to communicate with Authentik ID Provider
+/// </summary>
+public class AuthentikService : IIdentityProviderService
+{
+    public UserInfoDto? GetUserInfo(string accessHeader)
+    {
+        using HttpClient client = new HttpClient()
+        {
+            BaseAddress = new Uri(Environment.GetEnvironmentVariable("OPENID_BASE_URL"))
+        };
+
+        using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "userinfo");
+        request.Headers.Add("Authorization", accessHeader);
+
+        // TODO : Changer pour un comportement asynchrone
+        var response = client.SendAsync(request).Result;
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        return response.Content.ReadFromJsonAsync<UserInfoDto>(new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower }).Result;
+    }
+}
