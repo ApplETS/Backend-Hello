2ec63664e8076264e355f389077e02f2d198211e
superjekk <superjekk@outlook.com>
2026-01-01 19:54:15 -0500

    BUGFIX : Appel au point d'entrée Userinfo

--- core/Services/AuthentikService.cs
+++ core/Services/AuthentikService.cs
@@ -16,7 +16,7 @@ public class AuthentikService : IIdentityProviderService
             BaseAddress = new Uri(Environment.GetEnvironmentVariable("OPENID_BASE_URL"))
         };
 
-        using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "userinfo");
+        using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "userinfo/");
         request.Headers.Add("Authorization", accessHeader);
 
         // TODO : Changer pour un comportement asynchrone
